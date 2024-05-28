77729bbdfe1fa7f5517703e904fec1c48d4d7411
Nikola Lardev <nikolalardev@gmail.com>
2024-05-28 16:32:25 +0300

    Fix composer.json and remove console.log

--- view/frontend/web/js/clear-ref.js
+++ view/frontend/web/js/clear-ref.js
@@ -3,9 +3,7 @@ define(['jquery'], function ($) {
 
     return function () {
         $(document).ready(function () {
-            console.log('Before ref clear 1');
             window.Scout.clearStorage(window.Scout.storageDB, true);
-            console.log('After ref clear 2');
         });
     }
 });
